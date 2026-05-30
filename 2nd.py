6f9e5b7d0c9ac055e7bce93c8acaaaf8db693553
avikavish <avikavish1704@gmail.com>
2026-05-30 16:45:42 +0530

    Create 2nd.py

---
+++ 2nd.py
@@ -0,0 +1,11 @@
+# TRAFFIC LIGHT
+light=input("light:")
+if(light=="red"):
+    print("stop")
+elif(light=="green"):   
+    print("go")
+elif(light=="yellow"):
+    print("look")
+else:
+    print("light is broken")
+
