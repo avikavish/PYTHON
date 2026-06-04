0721d458413b3d87fb7d07f8978a62a1749f5f19
avikavish <avikavish1704@gmail.com>
2026-06-04 10:03:06 +0530

    Create 6th.py

---
+++ 6th.py
@@ -0,0 +1,16 @@
+
+# WAP to input 2 int ,a and b, and print true if a is greater than or equal to b , if not print false
+#  OR
+# greater of 2 number 
+
+
+a = int(input("enter the value of a :"))
+b = int(input("enter the value of b :"))
+
+if (a>b or a == b):
+    print("true")
+else:
+    print("false")
+
+ 
+# or print(a>=b)
