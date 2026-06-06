57de5b13aa9951987f5889d19fedfab24e475545
avikavish <avikavish1704@gmail.com>
2026-06-07 00:13:00 +0530

    Create 9th.py

---
+++ 9th.py
@@ -0,0 +1,10 @@
+
+#WAP to check if the user is eligible for votge of note
+
+age = input("enter your age:")
+
+if(age>="18"):
+    print("eligible for vote")
+
+else:
+    print("not eligible for vote")
