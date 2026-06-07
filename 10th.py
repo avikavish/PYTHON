bddf92a73611366dcbb91518f9af627255788869
avikavish <avikavish1704@gmail.com>
2026-06-08 00:03:32 +0530

    Create 10th.py

---
+++ 10th.py
@@ -0,0 +1,17 @@
+
+#wAP to check if the number enter by the user is odd or even 
+
+
+number = int(input("enter the number :"))
+
+rem = number % 2
+if(rem == 0):
+    print("numer is even")
+
+else:
+    print("odd")
+
+
+
+
+
