095fc6f9865052109bbc8c1a5dbfbb42e30a8d98
avikavish <avikavish1704@gmail.com>
2026-06-09 10:05:37 +0530

    Create 11th.py

---
+++ 11th.py
@@ -0,0 +1,18 @@
+
+#WAP to print the greatest of 3 number
+
+a = int(input("enter a:" ))
+b = int(input("enter b:" ))
+c = int(input("enter c:" ))
+
+if(a>=b and a>=c):
+    print(" a is greater",a)
+
+elif(b>=a and b>=c):
+    print(" b is greater",b)
+
+else:
+    print(" c is greater",c)
+
+
+
