bf443af657b9fb997f53dfd21c4388d349a7818f
avikavish <avikavish1704@gmail.com>
2026-06-10 14:11:42 +0530

    Create 12th.py

---
+++ 12th.py
@@ -0,0 +1,18 @@
+
+# grades of students
+
+marks = int(input("enter marks:"))
+
+if(marks>= 90):
+    print("grade A")
+
+elif(marks >= 80 and marks <= 90):
+    print("grade B")
+
+elif( marks >= 70 and  marks <= 80):
+    print(" grade C")
+
+elif(marks<= 70):
+    print(" grade D")
+
+
