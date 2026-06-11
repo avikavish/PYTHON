1998458a577999c3cd0b3fd82f31464b3b8011bd
avikavish <avikavish1704@gmail.com>
2026-06-11 15:19:30 +0530

    Create 13th.py

---
+++ 13th.py
@@ -0,0 +1,11 @@
+
+#WAP to check the numner is the multiple of 7 or not
+
+number = int(input("enter the number:"))
+rem = number % 7 
+
+if(rem == 0):
+    print(" multiple of 7")
+
+else:
+    print("not a multiple of 7")
