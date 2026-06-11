6ec449f02ab4af033c8f43ccf0b79e742b063d1f
avikavish <avikavish1704@gmail.com>
2026-06-12 00:27:04 +0530

    Create 14th.py

---
+++ 14th.py
@@ -0,0 +1,23 @@
+
+#wAP to enter three movie name and store them in a list 
+
+movie =[]
+mov1 = input("enter your first movie name:")
+mov2 = input("enter your second movie name:")
+mov3 = input("enter your third movie name:")
+
+movie.append(mov1)
+movie.append(mov2)
+movie.append(mov3)
+
+print(movie)
+
+
+# OR
+
+
+movie =[]
+movie.append = input("enter your first movie name:")
+movie.append = input("enter your second movie name:")
+movie.append = input("enter your third movie name:")
+print(movie)
