bbc502d009b7b1386b91e06612ad13bf29a27c7b
avikavish <avikavish1704@gmail.com>
2026-06-13 00:35:32 +0530

    Create 15th.py

---
+++ 15th.py
@@ -0,0 +1,16 @@
+#WAP to check if the user enter the palindrom value or not
+# PALINDROM
+
+list1 = ["1","2","3"]
+
+
+copy_list1=list1.copy( )
+copy_list1.reverse
+
+if(list1==copy_list1):
+    print("palindrom")
+
+else:
+    print("not palindrom")    
+
+
