43e351b262ada75cf74b9250ac95759a5c19fe3a
avikavish <avikavish1704@gmail.com>
2026-06-24 00:05:57 +0530

    Create 26.py

---
+++ 26.py
@@ -0,0 +1,19 @@
+
+#WAP to print sum of n natural number using for loop
+
+n = int(input("enter the number"))
+
+#sum = 0
+#for i in range(1,n+1):
+#    sum+=i
+#print("sum is =",sum)
+
+sum = 0
+i=0
+while(i<=n+1):
+    i+=1
+    sum+=i
+    print("sum is =",sum)
+
+    
+    
