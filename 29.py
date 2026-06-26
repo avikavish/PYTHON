8ce5c08e89bb0a0cd4bbab3adb0b47a64a8955f4
avikavish <avikavish1704@gmail.com>
2026-06-27 00:07:30 +0530

    Create 29.py

---
+++ 29.py
@@ -0,0 +1,14 @@
+
+# search the elements from following list
+#1 4 9 16 25 36 49 64 81 100
+
+nums =(1, 4, 9, 16, 25, 36, 49, 64, 81, 100)    # output will be index number
+x =9
+i = 0
+while i < len(nums):
+    if(nums[i]==x):
+        print("founded",i)
+    i+=1
+
+
+
