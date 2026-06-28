f1e0a0347c522bc801041fec37471af89912590c
avikavish <avikavish1704@gmail.com>
2026-06-29 00:03:33 +0530

    Create 31.py

---
+++ 31.py
@@ -0,0 +1,13 @@
+
+#WAP tp print the sum of n natural number(using while)
+
+num = int(input("enter number:"))
+sum = 0
+i = 1
+while i<=num:
+    sum= sum + i
+    i+=1
+
+print("total sum is:",sum)   
+
+
