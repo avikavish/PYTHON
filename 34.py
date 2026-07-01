973d42a77a5ca7a6429e30e1bf0f7231dae7ba1e
avikavish <avikavish1704@gmail.com>
2026-07-02 00:15:46 +0530

    Create 34.py

---
+++ 34.py
@@ -0,0 +1,15 @@
+cities =["jabalpur","bhopal" ,"indore"]
+heroes =["ironman", "thor","hulk"]
+
+def print_len(list):
+    print(len(list))
+
+def print_list(list):
+    for item in list:
+     print(item, end= " ")
+
+print_list(cities)
+
+
+
+
