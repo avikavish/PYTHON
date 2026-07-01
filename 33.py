b0884fc6c536c160df755268a04f318fee3a0776
avikavish <avikavish1704@gmail.com>
2026-07-01 20:53:17 +0530

    Create 33.py

---
+++ 33.py
@@ -0,0 +1,12 @@
+#FUNCTIONS
+#WAP to print the length of list (list is a parameter)
+
+cities =["jabalpur","bhopal" ,"indore"]
+heroes =["ironman", "thor","hulk"]
+
+def print_len(list):
+    print(len(list))
+
+    print_len(heroes)
+    print_len(cities)    
+    
